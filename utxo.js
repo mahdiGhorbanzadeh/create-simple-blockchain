365547b976b8f90ff391e2e8336b61ce413797a6
Mahdi Ghorbanzadeh <mahdigorbanzadeh.1378@yahoo.com>
2023-12-12 20:50:11 +0330

    add some function to utxo file

--- utxo.js
+++ utxo.js
@@ -15,18 +15,28 @@ class UTXOSet {
     const iterator = db.iterator({ gte: this.utxoPrefix });
 
     for await (const { key, value } of iterator) {
+      
       const txID = key.substring(this.utxoPrefix.length);
+
       const outs = deserializeOutputs(value);
 
-      for (let outIdx = 0; outIdx < outs.outputs.length; outIdx++) {
-        const out = outs.outputs[outIdx];
+      for (let outIdx = 0; outIdx < outs.length; outIdx++) {
+        const out = outs[outIdx];
 
         if (isLockedWithKey(out,pubKeyHash) && accumulated < amount) {
-          accumulated += out.Value;
+
+          accumulated += Number(out.Value);
+
           if (!unspentOuts[txID]) {
             unspentOuts[txID] = [];
           }
+
           unspentOuts[txID].push(outIdx);
+
+          if(accumulated>Number(amount)){
+            break;
+          }
+
         }
       }
     }
@@ -34,9 +44,114 @@ class UTXOSet {
     return { accumulated, unspentOuts };
   }
 
-//   serialize(block){
-//     return JSON.stringify(block)
-//   }
+
+  async findUTXO(pubKeyHash) {
+    let UTXOs = []
+    
+    // Use LevelDB iterator to traverse the database
+    const iterator = db.iterator({ gte: this.utxoPrefix });
+
+    for await (const { key, value } of iterator) {
+      
+      const txID = key.substring(this.utxoPrefix.length);
+
+      const outs = deserializeOutputs(value);
+
+      for (let outIdx = 0; outIdx < outs.length; outIdx++) {
+        const out = outs[outIdx];
+
+        if (isLockedWithKey(out,pubKeyHash)) {
+          if (!UTXOs[txID]) {
+            UTXOs[txID] = [];
+          }
+
+          UTXOs[txID].push(outIdx);
+        }
+      }
+    }
+
+    return UTXOs
+  }
+
+
+  async countTransactions(){
+    
+    let counter = 0;
+
+    const iterator = db.iterator({ gte: this.utxoPrefix });
+
+    for await (const { key } of iterator) {
+
+      if (!key.startsWith(utxoPrefix)) {
+        throw "error key was not have prefix !!!!"
+      }
+
+      counter++;
+    }
+
+    return counter;
+  }
+
+
+  async reIndex() {
+ 
+    const UTXO = this.blockchain.findUTXO(); 
+  
+    // Delete existing keys with the given prefix
+    await deleteByPrefix(utxoPrefix);
+  
+    
+    const batchOps = [];
+
+
+    for (let i = 0; i < UTXO.length; i++) {
+      const element = array[i];
+      
+    }
+  
+    for (const [txId, outs] of UTXO) {
+      const key = utxoPrefix + key;
+      batchOps.push({ type: 'put', key, value: serializeOutputs(outs) });
+    }
+  
+    await db.batch(batchOps);
+  }
+
+
+  async deleteByPrefix(prefix) {
+
+    const deleteKeys = async (keysForDelete) => {
+      await db.batch(keysForDelete.map(key => ({ type: 'del', key })));
+    };
+  
+    const collectSize = 100000;
+    const keysForDelete = [];
+  
+    const iterator = db.iterator({ gte: prefix });
+  
+    for await (const { key } of iterator) {
+      if (!key.startsWith(prefix)) {
+        throw "error key was not have prefix !!!! (deleteByPrefix)"
+      }
+  
+      keysForDelete.push(key);
+  
+      if (keysForDelete.length === collectSize) {
+        await deleteKeys(keysForDelete);
+        keysForDelete.length = 0; // Reset the array
+      }
+    }
+  
+    // Delete remaining keys
+    if (keysForDelete.length > 0) {
+      await deleteKeys(keysForDelete);
+    }
+  }
+
+
+
+
+
 
   deserializeOutputs(outputs){
     return JSON.parse(outputs)
